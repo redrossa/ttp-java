c6968380915979f96279316726546c31bd9dba78
redrossa <adriano.raksi@gmail.com>
2019-07-11 13:57:26 -0500

    Add more constructors and change field modifiers

--- src/main/java/com/github/redrossa/ttp/Packet.java
+++ src/main/java/com/github/redrossa/ttp/Packet.java
@@ -1,6 +1,5 @@
 package com.github.redrossa.ttp;
 
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
@@ -8,48 +7,53 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 /**
- * In TTP, data are transferred into packets. A packet is structured into
- * two main components: header and body. There are four data types in TTP;
- * they are boolean, integer, double and string. These data types are stored
- * in byte arrays using the representation of their respective String UTF-8
- * values. The body component of a packet holds this byte array, while the
- * header component indicates which of the four TTP data types does the body
- * hold. The header is an {@code int} mask of a Header enum.
+ * A {@code Packet} provides for organising data and extending its customisability.
+ * In a packet, data are structured into three components: header, body and footer.
+ * <p>
+ * The header is a 32-bit integer value that indicates what type of packet this is
+ * and what can be generalised about the data that follows: the body.
+ * </p>
+ * The body is the main, information-rich data component. This is where the data which the
+ * programmer intends to deliver is stored.
+ * <p>
+ * Like the header, the footer is another value that holds information about the packet
+ * and essentially the body. But unlike the header, which is restricted to implementations
+ * of {@link Headerable}, the footer provides for higher customisability, where the programmer
+ * specifies and parses the value in their own programmes themselves.
+ * </p>
  *
  * @author  Adriano Raksi
  * @version 1.0-SNAPSHOT
  * @since   2019-06-19
  */
-public class Packet implements Serializable
+public final class Packet implements Serializable
 {
-    /** Maximum byte size of a Packet object */
-    public static final int MAX_SIZE = 256;
-
-    /** Minimum byte size of a Packet object */
-    public static final int MIN_SIZE = 16;
-
     /** Class serial version UID */
     private static final long serialVersionUID = 7474662397063358803L;
 
-    /** Mask of the Header enum of this packet */
-    public final int header;
+    /** Header value */
+    final int header;
 
-    /** Byte array representation of the the String value of the accepted data type */
-    public final byte[] body;
+    /** Byte array UTF-8 encoding of a {@code String} value */
+    final byte[] body;
 
-    /** Unsigned 16-bit type to store further customisation of a packet */
-    public final char footer;
+    /** Unsigned 16-bit type to store further customisation of a {@code Packet} */
+    final char footer;
 
-    /** Default constructor creates a no-operation packet. */
+    /** Default constructor creates a no-operation {@code Packet}. */
     public Packet()
     {
         this(Header.NOP, "", 0);
     }
 
     /**
-     * Creates a new packet object of type boolean and default {@code 0} footer.
+     * Creates a {@code Packet} of type {@link Header#BOOLEAN} and default {@code 0} footer.
+     * <p>
+     * The {@code double}
+     * value is converted to {@code String} to be encoded in UTF-8.
+     * </p>
      *
-     * @param   val {@code boolean} value of the data of the packet.
+     * @param val the {@code double} value for the body.
      */
     public Packet(boolean val)
     {
@@ -57,9 +61,28 @@ public class Packet implements Serializable
     }
 
     /**
-     * Creates a new packet of type integer and default {@code 0} footer.
+     * Creates a {@code Packet} of type {@link Header#BOOLEAN} and footer specified.
+     * <p>
+     * The {@code double}
+     * value is converted to {@code String} to be encoded in UTF-8.
+     * </p>
      *
-     * @param   val {@code int} value of the data of the packet.
+     * @param val    the {@code double} value for the body.
+     * @param footer the footer value
+     */
+    public Packet(boolean val, int footer)
+    {
+        this(Header.DOUBLE, String.valueOf(val), footer);
+    }
+
+    /**
+     * Creates a {@code Packet} of type {@link Header#INTEGER} and default {@code 0} footer.
+     * <p>
+     * The {@code double}
+     * value is converted to {@code String} to be encoded in UTF-8.
+     * </p>
+     *
+     * @param val the {@code double} value for the body.
      */
     public Packet(int val)
     {
@@ -67,9 +90,28 @@ public class Packet implements Serializable
     }
 
     /**
-     * Creates a new packet of type double and default {@code 0} footer.
+     * Creates a {@code Packet} of type {@link Header#INTEGER} and footer specified.
+     * <p>
+     * The {@code double}
+     * value is converted to {@code String} to be encoded in UTF-8.
+     * </p>
      *
-     * @param   val {@code double} value of the data of the packet.
+     * @param val    the {@code double} value for the body.
+     * @param footer the footer value
+     */
+    public Packet(int val, int footer)
+    {
+        this(Header.DOUBLE, String.valueOf(val), footer);
+    }
+
+    /**
+     * Creates a {@code Packet} of type {@link Header#DOUBLE} and default {@code 0} footer.
+     * <p>
+     * The {@code double}
+     * value is converted to {@code String} to be encoded in UTF-8.
+     * </p>
+     *
+     * @param val the {@code double} value for the body.
      */
     public Packet(double val)
     {
@@ -77,55 +119,100 @@ public class Packet implements Serializable
     }
 
     /**
-     * Creates a new packet of type string and default {@code 0} footer.
+     * Creates a {@code Packet} of type {@link Header#DOUBLE} and footer specified.
+     * <p>
+     * The {@code double}
+     * value is converted to {@code String} to be encoded in UTF-8.
+     * </p>
      *
-     * @param   val {@code String} value of the data of the packet.
+     * @param val    the {@code double} value for the body.
+     * @param footer the footer value
      */
-    public Packet(@NotNull String val)
+    public Packet(double val, int footer)
     {
-        this(Header.STRING, val, 0);
+        this(Header.DOUBLE, String.valueOf(val), footer);
     }
 
     /**
-     * Creates a new packet of type {@code header}, of String
-     * {@code body} and of unsigned 16-bit {@code footer} specified. This
-     * constructor guarantees the header mask specified of this packet exists and
-     * that the body is universally encoded.
+     * Creates a {@code Packet} of type {@link Header#STRING} and default {@code 0} footer.
+     * <p>
+     * The {@code String}
+     * value is to be encoded in UTF-8.
+     * </p>
      *
-     * @param   header  Header object to indicate the packet type.
-     * @param   body    String body, which will be encoded into bytes. This way,
-     *                  the byte encoding of the body is only of UTF-8.
-     * @param   footer  extra minimal information of the packet.
+     * @param val the {@code String} value for the body.
      */
-    @Contract(value = "null, _, _ -> fail; !null, null, _ -> fail", pure = true)
+    public Packet(@NotNull String val)
+    {
+        this(val, 0);
+    }
+
+    /**
+     * Creates a {@code Packet} of type {@link Header#STRING} and footer specified.
+     * <p>
+     * The {@code String}
+     * value is to be encoded in UTF-8.
+     * </p>
+     *
+     * @param val    the {@code String} value for the body.
+     * @param footer the footer value.
+     */
+    public Packet(@NotNull String val, int footer)
+    {
+        this(Header.STRING, val, footer);
+    }
+
+    /**
+     * Creates a {@code Packet} out of complex objects specified.
+     * <p>
+     * Used by classes outside of this package, this constructor makes sure
+     * the would-be header value is appropriate, either it is a {@link Header}
+     * enum or it is a custom implementor of the {@link Headerable} object.
+     * This constructor also makes sure that the data body of this packet is
+     * encoded in UTF-8 encoding, by encoding the data body in this constructor
+     * itself rather than by the caller. Agreeing to the contract, a packet body
+     * must always be encoded in UTF-8.
+     * </p>
+     *
+     * @param header the header value.
+     * @param body   the body value.
+     * @param footer the footer value.
+     */
     public Packet(@NotNull Headerable header, @NotNull String body, int footer)
     {
         this(header.getMask(), body.getBytes(StandardCharsets.UTF_8), (char) footer);
     }
 
     /**
-     * Creates a new packet with the raw data specified. This constructor
-     * assumes the caller has already checked that their parameter is appropriate
-     * for a packet.
+     * Creates a {@code Packet} out of raw data specified.
+     * <p>
+     * Used by classes in the same package only, this constructor assumes arguments
+     * passed by the caller are appropriate for constructing this packet object.
+     * It is the responsibility of the caller to make sure the arguments passed
+     * to this constructor are appropriate.
+     * </p>
      *
-     * @param   header  header mask of this packet.
-     * @param   body    body of this packet.
-     * @param   footer  footer of this packet.
+     * @param header the header value.
+     * @param body   the body data.
+     * @param footer the footer value.
      */
     Packet(int header, byte[] body, char footer)
     {
+        if (body == null)
+            throw new NullPointerException();
         this.header = header;
         this.body = body;
         this.footer = footer;
     }
 
     /**
-     * Returns the hashcode customised for class Packet. The hashcode
-     * of Packet depends on {@code header} and the hashcode of array
-     * {@code body}.
+     * Returns the hashcode customised for this {@code Packet}.
+     * <p>
+     * The hashcode
+     * depends on the fields of this {@code Packet}.
+     * </p>
      *
-     * @return  hashcode of this packet.
-     * @see     Object#hashCode()
+     * @return hashcode of this {@code Packet}.
      */
     @Override
     public int hashCode()
@@ -138,14 +225,12 @@ public class Packet implements Serializable
     }
 
     /**
-     * Indicates whether this packet is "equal" to {@code obj}. This method
-     * is overridden to take in consideration of the instances of this Packet
-     * object.
+     * Indicates whether this {@code Packet} is "equal" to {@code obj}. This method
+     * is overridden to take in consideration of the fields of this {@code Packet}.
      *
-     * @param   obj     the reference object with which to compare.
-     * @return  {@code true} if this object is the same as the obj
-     *          argument; {@code false} otherwise.
-     * @see     Object#equals(Object)
+     * @param obj the reference object with which to compare.
+     * @return {@code true} if this object is the same as the obj
+     *         argument; {@code false} otherwise.
      */
     @Override
     public boolean equals(Object obj)
@@ -161,10 +246,14 @@ public class Packet implements Serializable
     }
 
     /**
-     * Returns the string representation of this packet.
+     * Returns a string representation of this {@code Packet}.
+     * <p>
+     * The string representation consists of this {@code Packet}'s fields: header value,
+     * body in {@code String} format and footer value. The fields are enclosed in square
+     * brackets ("{@code []}") and separated by "{@code / }" (space, forward slash, space).
+     * </p>
      *
-     * @return  a string representation of this packet.
-     * @see     Object#toString()
+     * @return a string representation of this {@code Packet}.
      */
     @Override
     public String toString()
@@ -173,12 +262,46 @@ public class Packet implements Serializable
     }
 
     /**
-     * Returns the encoded body byte array in UTF-8.
+     * Returns the UTF-8-decoded {@code body} data byte array.
+     * <p>
+     * Encoding should never fail because {@code body} is always encoded in UTF-8. A failure
+     * indicates an invalid creation of {@code Packet} outside of this package.
+     * </p>
      *
-     * @return  the encoded body byte array in UTF-8
+     * @return the UTF-8-decoded {@code body} data byte array.
      */
     public String format()
     {
         return new String(body, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * Returns the {@code header} value.
+     *
+     * @return the {@code header} value.
+     */
+    public int header()
+    {
+        return header;
+    }
+
+    /**
+     * Returns the UTF-8 encoded {@code body} data byte array.
+     *
+     * @return the UTF-8 encoded {@code body} data byte array.
+     */
+    public byte[] body()
+    {
+        return body;
+    }
+
+    /**
+     * Returns the {@code footer} value.
+     *
+     * @return the {@code footer} value.
+     */
+    public char footer()
+    {
+        return footer;
     }
 }
