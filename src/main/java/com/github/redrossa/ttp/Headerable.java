110632196169d742d544ce8f8cce47873b0734b9
redrossa <adriano.raksi@gmail.com>
2019-07-11 23:49:19 -0500

    Fix documentation

--- src/main/java/com/github/redrossa/ttp/Headerable.java
+++ src/main/java/com/github/redrossa/ttp/Headerable.java
@@ -5,41 +5,64 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 
 /**
- * The header indicates the standardised type of the data body of
- * a TTP packet. A mask is a three-digit integer value of a header.
- * The first digit indicates category, and the last two digits are
- * used for the actual distinction. 0XX are operations, 1XX are
- * types to be converted into the implementor's language of use and
- * 2XX are binary variables for intercommunication between implementors.
- * Masks that are written with less than three digits are assumed to be
- * of the operation category.
+ * This interface provides the usability of an instance class or
+ * enum as a header of a {@link Packet}. Implementation of this
+ * interface must declare a field in their instances that holds a
+ * unique {@code int} value called a mask. Ideally, the instances
+ * must be static and their respective masks unique from other
+ * masks of other instances of that same class or enum. Additionally,
+ * the class or enum must provide an instance implementation of the
+ * {@link Headerable#getMask()} which returns its mask value.
  * <p>
- * Enum types that implement this interface may be used as headers of
- * TTP packet, allowing an extensible enum feature for custom headers.
+ * A mask, in decimal, is composed of three digits. The first digit
+ * indicates the category and the last two indicates the value of
+ * that specific header. So in one category, there can only be a
+ * maximum of 100 types of headers (from X00 to X99 inclusive), and
+ * in a {@code Headerable} implementation, there can only be a maximum
+ * of 10 types of categories (from 0XX to 9XX inclusive). This
+ * information of organising into categories is not, however, stored
+ * by the {@code Packet}, not to mention the class of the implementation
+ * of this interface. Therefore, it is up to the programmer to decode
+ * into what type of {@code Headerable} implementation is the
+ * {@code Packet} delivering.
+ * <p>
+ * The {@link Headerable#valueOf(int, Class)} method allows the programmer
+ * to retrieve which instance in the specified {@code Headerable} class
+ * implementation is the header of the {@code Packet}, especially since
+ * the header information is stored in the {@code Packet} raw (in
+ * {@code int}). It is recommended for classes or enums implementing this
+ * interface to provide an implementation of their own {@code valueOf} method
+ * which may call {@code Headerable}'s {@code valueOf} method that passes its
+ * own class and returns the result.
  *
  * @author  Adriano Raksi
  * @version 1.0-SNAPSHOT
  * @since   2019-06-29
+ * @see     Header
  */
 public interface Headerable
 {
     /**
-     * Returns this Header enum's mask.
+     * Returns the unique mask of this header instance.
      *
-     * @return  this header's mask.
+     * @return the mask.
      */
     int getMask();
 
     /**
-     * Returns the header enum associated with {@code mask}
-     * and the enum class that implements this interface.
-     * If no enum is associated with it, null is returned.
+     * Returns the header instance associated with the specified mask
+     * of the specified class that implements this interface. If no
+     * mask is associated with a header instance in the specified class,
+     * null is returned.
+     * <p>
+     * It is recommended for classes or enums implementing this
+     * interface to provide an implementation of their own {@code valueOf} method
+     * which may call this method that passes its own class and returns the result.
      *
-     * @param   mask    an {@code int} value whose associated enum is
-     *                  to be returned.
-     * @param   clazz   the enum that implements this interface.
-     * @return  the header enum associated with {@code mask} or null
-     *          if not found.
+     * @param  mask the mask to find with which a header is associated.
+     * @param  clazz the class that implements this interface.
+     * @return the header instance associated with the specified mask
+     *         or null if not found.
      */
     static <T extends Headerable> T valueOf(int mask, @NotNull Class<T> clazz)
     {
